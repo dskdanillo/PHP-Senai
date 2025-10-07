89c4598c6f005a6d16e229b4783312d447affbb3
dskdanillo <dsk.danillo@gmail.com>
2025-10-06 21:54:59 -0300

    correçÃO DA LINHA 11 DO php ESPAÇO ENTRE O  e o request

--- aplic2php/js/validacoes.js
+++ aplic2php/js/validacoes.js
@@ -1,29 +1,27 @@
 function validarFormulario() {
+
+
+    
+    let camponome = document.getElementById("nome");
     let nome = document.getElementById("nome").value.trim();
+   
+    let campoemail = document.getElementById("email");
     let email = document.getElementById("email").value.trim();
-    if (nome ==="" || email ==="") {
-
-        alert("Preencha todos os campos obrigatórios");
-        return false;
-    }
-
-    return true;
-
-}
-
-/*function validarFormulario() {
-    let nome = document.getElementById("nome").value.trim();
-    let email = document.getElementById("email").value.trim();
+    
+    
+    
     if (nome === "") {
 
         alert("Preencha o campo Nome obrigatórios");
+        camponome.focus();
         return false;
     }
     else if (email === "") {
         alert("Preencha o campo email obrigatórios");
+        campoemail.focus();
         return false;
 
     }
     return true;
 
-}*/
+}
--- aplic2php/processa.php
+++ aplic2php/processa.php
@@ -8,29 +8,29 @@ function sair (){
 }
 
 //------------------------------------------------------
-if($_SERVER[' RESQUEST_METHOD']==='POST'){
-    $nome = $_POST['nome'] ?? '';
-    $email = $_POST['email'] ?? '';
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    $nome = $_POST['txnome'] ?? '';
+    $email = $_POST['txemail'] ?? '';
 
     //verifica se o campo está vazio
 
     if(empty($nome)){
 
         echo "O Campo nome está vazio.<br>";
-        echo "<br><a href='index.html'>Voltar</a>";
+        
         sair();//chamando a função sair
 
     }else if(empty($email)){
 
         echo "O campo email está vazio.<br>";
-        echo "<br><a href ='index.html'>Voltar</a>";
-        sair();//chamando a função sair
         
+        sair();//chamando a função sair
+
     }else if(!empty($nome)&& ! empty($email)){
 
         echo "Nome : ". htmlspecialchars($nome). "<br>";
         echo "Email ; ". htmlspecialchars($email). "<br>";
-        echo "<br><a href= 'index.html'>Voltar</a>";
+        
         sair();//chamando a função sair
 
 
